cf72fd54c9d71a23a001b485fa01dd303b15abe9
Eric Vlaskin <eric@vlaskin.org>
2023-05-14 01:34:20 +0300

    version 1 (tests)

---
+++ src/test/java/org/vlaskin/moexiss/service/TestUtils.java
@@ -0,0 +1,13 @@
+package org.vlaskin.moexiss.service;
+
+public class TestUtils
+{
+    public static final String SECURITY = "YNDX";
+
+    public static final String ENGINE = "stock";
+    public static final String MARKET = "shares";
+    public static final String BOARD = "TQBR";
+
+    public static final String INDEX = "IMOEX";
+    public static final String TICKER = SECURITY;
+}

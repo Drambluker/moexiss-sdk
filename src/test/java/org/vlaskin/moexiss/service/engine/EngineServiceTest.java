6fe2c34e0935f47f59ba16a7a52456afecc8cf5b
Eric Vlaskin <eric@vlaskin.org>
2023-07-15 02:54:08 +0300

    version 1 (tests)

--- src/test/java/org/vlaskin/moexiss/service/engine/EngineServiceTest.java
+++ src/test/java/org/vlaskin/moexiss/service/engine/EngineServiceTest.java
@@ -3,6 +3,7 @@ package org.vlaskin.moexiss.service.engine;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.vlaskin.moexiss.MoexClient;
+import org.vlaskin.moexiss.entity.MarketResponse;
 import org.vlaskin.moexiss.service.TestUtils;
 import org.vlaskin.moexiss.service.engine.params.BoardEngineParams;
 import org.vlaskin.moexiss.service.engine.params.BoardSecuritiesTableEngineParams;
@@ -27,10 +28,7 @@ class EngineServiceTest
     private static final ListEngineParams listParams = new ListEngineParams();
     private static final InfoEngineParams infoParams = new InfoEngineParams(TestUtils.ENGINE);
     private static final MarketsEngineParams marketsParams = new MarketsEngineParams(TestUtils.ENGINE);
-    private static final MarketInfoEngineParams marketInfoParams = new MarketInfoEngineParams(TestUtils.ENGINE, TestUtils.MARKET);
-    private static final MarketSecuritiesTableEngineParams marketSecuritiesTableParams = new MarketSecuritiesTableEngineParams(TestUtils.ENGINE, TestUtils.MARKET);
     private static final MarketSecurityTableEngineParams marketSecurityTableParams = new MarketSecurityTableEngineParams(TestUtils.ENGINE, TestUtils.MARKET, TestUtils.SECURITY);
-    private static final BoardsEngineParams boardsParams = new BoardsEngineParams(TestUtils.ENGINE, TestUtils.MARKET);
     private static final BoardEngineParams boardParams = new BoardEngineParams(TestUtils.ENGINE, TestUtils.MARKET, TestUtils.BOARD);
     private static final BoardSecuritiesTableEngineParams boardSecuritiesTableParams = new BoardSecuritiesTableEngineParams(TestUtils.ENGINE, TestUtils.MARKET, TestUtils.BOARD);
     private static final BoardSecurityTableEngineParams boardSecurityTableParams = new BoardSecurityTableEngineParams(TestUtils.ENGINE, TestUtils.MARKET, TestUtils.BOARD, TestUtils.SECURITY);
@@ -38,196 +36,234 @@ class EngineServiceTest
     @Test
     void getList() throws IOException
     {
-        log.info(service.getList(listParams).toString());
+        log.debug(service.getList(listParams).toString());
     }
 
     @Test
     void getInfo() throws IOException
     {
-        log.info(service.getInfo(infoParams).toString());
+        log.debug(service.getInfo(infoParams).toString());
     }
 
     @Test
     void getEngineInfo() throws IOException
     {
-        log.info(service.getEngineInfo(infoParams).toString());
+        log.debug(service.getEngineInfo(infoParams).toString());
     }
 
     @Test
     void getTimeTableInfo() throws IOException
     {
-        log.info(service.getTimeTableInfo(infoParams).toString());
+        log.debug(service.getTimeTableInfo(infoParams).toString());
     }
 
     @Test
     void getDailyTableInfo() throws IOException
     {
-        log.info(service.getDailyTableInfo(infoParams).toString());
+        log.debug(service.getDailyTableInfo(infoParams).toString());
     }
 
     @Test
     void getMarkets() throws IOException
     {
-        log.info(service.getMarkets(marketsParams).toString());
+        log.debug(service.getMarkets(marketsParams).toString());
     }
 
     @Test
     void getMarketInfo() throws IOException
     {
-        log.info(service.getMarketInfo(marketInfoParams).toString());
+        Iterable<MarketResponse> markets = service.getMarkets(marketsParams);
+        for (MarketResponse market : markets)
+            log.debug(service.getMarketInfo(new MarketInfoEngineParams(TestUtils.ENGINE, market.getStringFields().get(MarketResponse.Fields.NAME))).toString());
     }
 
     @Test
     void getMarketBoards() throws IOException
     {
-        log.info(service.getMarketBoards(marketInfoParams).toString());
+        Iterable<MarketResponse> markets = service.getMarkets(marketsParams);
+        for (MarketResponse market : markets)
+            log.debug(service.getMarketBoards(new MarketInfoEngineParams(TestUtils.ENGINE, market.getStringFields().get(MarketResponse.Fields.NAME))).toString());
     }
 
     @Test
     void getMarketBoardGroups() throws IOException
     {
-        log.info(service.getMarketBoardGroups(marketInfoParams).toString());
+        Iterable<MarketResponse> markets = service.getMarkets(marketsParams);
+        for (MarketResponse market : markets)
+            log.debug(service.getMarketBoardGroups(new MarketInfoEngineParams(TestUtils.ENGINE, market.getStringFields().get(MarketResponse.Fields.NAME))).toString());
     }
 
     @Test
     void getMarketSecurityFields() throws IOException
     {
-        log.info(service.getMarketSecurityFields(marketInfoParams).toString());
+        Iterable<MarketResponse> markets = service.getMarkets(marketsParams);
+        for (MarketResponse market : markets)
+            log.debug(service.getMarketSecurityFields(new MarketInfoEngineParams(TestUtils.ENGINE, market.getStringFields().get(MarketResponse.Fields.NAME))).toString());
     }
 
     @Test
     void getMarketDataFields() throws IOException
     {
-        log.info(service.getMarketDataFields(marketInfoParams).toString());
+        Iterable<MarketResponse> markets = service.getMarkets(marketsParams);
+        for (MarketResponse market : markets)
+            log.debug(service.getMarketDataFields(new MarketInfoEngineParams(TestUtils.ENGINE, market.getStringFields().get(MarketResponse.Fields.NAME))).toString());
     }
 
     @Test
     void getMarketTradeFields() throws IOException
     {
-        log.info(service.getMarketTradeFields(marketInfoParams).toString());
+        Iterable<MarketResponse> markets = service.getMarkets(marketsParams);
+        for (MarketResponse market : markets)
+            log.debug(service.getMarketTradeFields(new MarketInfoEngineParams(TestUtils.ENGINE, market.getStringFields().get(MarketResponse.Fields.NAME))).toString());
     }
 
     @Test
     void getMarketOrderBookFields() throws IOException
     {
-        log.info(service.getMarketOrderBookFields(marketInfoParams).toString());
+        Iterable<MarketResponse> markets = service.getMarkets(marketsParams);
+        for (MarketResponse market : markets)
+            log.debug(service.getMarketOrderBookFields(new MarketInfoEngineParams(TestUtils.ENGINE, market.getStringFields().get(MarketResponse.Fields.NAME))).toString());
     }
 
     @Test
     void getMarketHistoryFields() throws IOException
     {
-        log.info(service.getMarketHistoryFields(marketInfoParams).toString());
+        Iterable<MarketResponse> markets = service.getMarkets(marketsParams);
+        for (MarketResponse market : markets)
+            log.debug(service.getMarketHistoryFields(new MarketInfoEngineParams(TestUtils.ENGINE, market.getStringFields().get(MarketResponse.Fields.NAME))).toString());
     }
 
     @Test
     void getMarketTradeHistoryFields() throws IOException
     {
-        log.info(service.getMarketTradeHistoryFields(marketInfoParams).toString());
+        Iterable<MarketResponse> markets = service.getMarkets(marketsParams);
+        for (MarketResponse market : markets)
+            log.debug(service.getMarketTradeHistoryFields(new MarketInfoEngineParams(TestUtils.ENGINE, market.getStringFields().get(MarketResponse.Fields.NAME))).toString());
     }
 
     @Test
     void getMarketDataYieldFields() throws IOException
     {
-        log.info(service.getMarketDataYieldFields(marketInfoParams).toString());
+        Iterable<MarketResponse> markets = service.getMarkets(marketsParams);
+        for (MarketResponse market : markets)
+            log.debug(service.getMarketDataYieldFields(new MarketInfoEngineParams(TestUtils.ENGINE, market.getStringFields().get(MarketResponse.Fields.NAME))).toString());
     }
 
     @Test
     void getMarketTradeYieldFields() throws IOException
     {
-        log.info(service.getMarketTradeYieldFields(marketInfoParams).toString());
+        Iterable<MarketResponse> markets = service.getMarkets(marketsParams);
+        for (MarketResponse market : markets)
+            log.debug(service.getMarketTradeYieldFields(new MarketInfoEngineParams(TestUtils.ENGINE, market.getStringFields().get(MarketResponse.Fields.NAME))).toString());
     }
 
     @Test
     void getMarketHistoryYieldFields() throws IOException
     {
-        log.info(service.getMarketHistoryYieldFields(marketInfoParams).toString());
+        Iterable<MarketResponse> markets = service.getMarkets(marketsParams);
+        for (MarketResponse market : markets)
+            log.debug(service.getMarketHistoryYieldFields(new MarketInfoEngineParams(TestUtils.ENGINE, market.getStringFields().get(MarketResponse.Fields.NAME))).toString());
     }
 
     @Test
     void getMarketSecurityStatisticFields() throws IOException
     {
-        log.info(service.getMarketSecurityStatisticFields(marketInfoParams).toString());
+        Iterable<MarketResponse> markets = service.getMarkets(marketsParams);
+        for (MarketResponse market : markets)
+            log.debug(service.getMarketSecurityStatisticFields(new MarketInfoEngineParams(TestUtils.ENGINE, market.getStringFields().get(MarketResponse.Fields.NAME))).toString());
     }
 
     @Test
     void getMarketSecuritiesTable() throws IOException
     {
-        log.info(service.getMarketSecuritiesTable(marketSecuritiesTableParams).toString());
-        log.info(service.getMarketSecuritiesTable(marketSecurityTableParams).toString());
+        Iterable<MarketResponse> markets = service.getMarkets(marketsParams);
+        for (MarketResponse market : markets)
+            log.debug(service.getMarketSecuritiesTable(new MarketSecuritiesTableEngineParams(TestUtils.ENGINE, market.getStringFields().get(MarketResponse.Fields.NAME))).toString());
+        log.debug(service.getMarketSecuritiesTable(marketSecurityTableParams).toString());
     }
 
     @Test
     void getMarketSecurities() throws IOException
     {
-        log.info(service.getMarketSecurities(marketSecuritiesTableParams).toString());
-        log.info(service.getMarketSecurities(marketSecurityTableParams).toString());
+        Iterable<MarketResponse> markets = service.getMarkets(marketsParams);
+        for (MarketResponse market : markets)
+            log.debug(service.getMarketSecurities(new MarketSecuritiesTableEngineParams(TestUtils.ENGINE, market.getStringFields().get(MarketResponse.Fields.NAME))).toString());
+        log.debug(service.getMarketSecurities(marketSecurityTableParams).toString());
     }
 
     @Test
     void getMarketData() throws IOException
     {
-        log.info(service.getMarketData(marketSecuritiesTableParams).toString());
-        log.info(service.getMarketData(marketSecurityTableParams).toString());
+        Iterable<MarketResponse> markets = service.getMarkets(marketsParams);
+        for (MarketResponse market : markets)
+            log.debug(service.getMarketData(new MarketSecuritiesTableEngineParams(TestUtils.ENGINE, market.getStringFields().get(MarketResponse.Fields.NAME))).toString());
+        log.debug(service.getMarketData(marketSecurityTableParams).toString());
     }
 
     @Test
     void getMarketDataVersions() throws IOException
     {
-        log.info(service.getMarketDataVersions(marketSecuritiesTableParams).toString());
-        log.info(service.getMarketDataVersions(marketSecurityTableParams).toString());
+        Iterable<MarketResponse> markets = service.getMarkets(marketsParams);
+        for (MarketResponse market : markets)
+            log.debug(service.getMarketDataVersions(new MarketSecuritiesTableEngineParams(TestUtils.ENGINE, market.getStringFields().get(MarketResponse.Fields.NAME))).toString());
+        log.debug(service.getMarketDataVersions(marketSecurityTableParams).toString());
     }
 
     @Test
     void getMarketDataYields() throws IOException
     {
-        log.info(service.getMarketDataYields(marketSecuritiesTableParams).toString());
-        log.info(service.getMarketDataYields(marketSecurityTableParams).toString());
+        Iterable<MarketResponse> markets = service.getMarkets(marketsParams);
+        for (MarketResponse market : markets)
+            log.debug(service.getMarketDataYields(new MarketSecuritiesTableEngineParams(TestUtils.ENGINE, market.getStringFields().get(MarketResponse.Fields.NAME))).toString());
+        log.debug(service.getMarketDataYields(marketSecurityTableParams).toString());
     }
 
     @Test
     void getBoards() throws IOException
     {
-        log.info(service.getBoards(boardsParams).toString());
+        Iterable<MarketResponse> markets = service.getMarkets(marketsParams);
+        for (MarketResponse market : markets)
+            log.debug(service.getBoards(new BoardsEngineParams(TestUtils.ENGINE, market.getStringFields().get(MarketResponse.Fields.NAME))).toString());
     }
 
     @Test
     void getBoard() throws IOException
     {
-        log.info(service.getBoard(boardParams).toString());
+        log.debug(service.getBoard(boardParams).toString());
     }
 
     @Test
     void getBoardSecuritiesTable() throws IOException
     {
-        log.info(service.getBoardSecuritiesTable(boardSecuritiesTableParams).toString());
-        log.info(service.getBoardSecurityTable(boardSecurityTableParams).toString());
+        log.debug(service.getBoardSecuritiesTable(boardSecuritiesTableParams).toString());
+        log.debug(service.getBoardSecurityTable(boardSecurityTableParams).toString());
     }
 
     @Test
     void getBoardSecurities() throws IOException
     {
-        log.info(service.getBoardSecurities(boardSecuritiesTableParams).toString());
-        log.info(service.getBoardSecurity(boardSecurityTableParams).toString());
+        log.debug(service.getBoardSecurities(boardSecuritiesTableParams).toString());
+        log.debug(service.getBoardSecurity(boardSecurityTableParams).toString());
     }
 
     @Test
     void getBoardMarketData() throws IOException
     {
-        log.info(service.getBoardMarketData(boardSecuritiesTableParams).toString());
-        log.info(service.getBoardMarketData(boardSecurityTableParams).toString());
+        log.debug(service.getBoardMarketData(boardSecuritiesTableParams).toString());
+        log.debug(service.getBoardMarketData(boardSecurityTableParams).toString());
     }
 
     @Test
     void getBoardDataVersions() throws IOException
     {
-        log.info(service.getBoardDataVersions(boardSecuritiesTableParams).toString());
-        log.info(service.getBoardDataVersions(boardSecurityTableParams).toString());
+        log.debug(service.getBoardDataVersions(boardSecuritiesTableParams).toString());
+        log.debug(service.getBoardDataVersions(boardSecurityTableParams).toString());
     }
 
     @Test
     void getBoardMarketDataYields() throws IOException
     {
-        log.info(service.getBoardMarketDataYields(boardSecuritiesTableParams).toString());
-        log.info(service.getBoardMarketDataYields(boardSecurityTableParams).toString());
+        log.debug(service.getBoardMarketDataYields(boardSecuritiesTableParams).toString());
+        log.debug(service.getBoardMarketDataYields(boardSecurityTableParams).toString());
     }
 }
